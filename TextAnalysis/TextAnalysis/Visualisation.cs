000357804798efebc8e989956a21c17386c93725
KallumP <kallum179@gmail.com>
2022-04-12 16:12:11 +0100

    Can now click on definition vectors to visualise that vector, definitions are now centered properly

--- TextAnalysis/TextAnalysis/Visualisation.cs
+++ TextAnalysis/TextAnalysis/Visualisation.cs
@@ -16,11 +16,12 @@ namespace TextAnalysis {
         public Visualisation(int startingVector) {
             InitializeComponent();
             vector = startingVector;
+            WindowState = FormWindowState.Maximized;
         }
 
         private void Visualisation_Paint(object sender, PaintEventArgs e) {
             //label1.Text = mousePos.X.ToString() + ", " + mousePos.Y.ToString();
-            
+
             //updates which vector is being visualised
             currentVector_lbl.Text = "Vector being visualised: " + vector.ToString() + ", " + TextAnalysis.Text.vectors[vector].word;
 
@@ -61,11 +62,11 @@ namespace TextAnalysis {
         void DrawDefinition(Graphics g, int definitionIndex, int totalDefinitions) {
 
 
-            int fontSize = 7;
+            int fontSize = 8;
             string font = "Consolas";
             Font f = new Font(font, fontSize);
 
-            int distanceFromCenter = 250;
+            int distanceFromCenter = fontSize * 51;
             Point center = new Point(Width / 2, Height / 2);
             float ratio = (definitionIndex + 1f) / totalDefinitions;
             float angle = (float)(ratio * Math.PI * 2);
@@ -74,23 +75,41 @@ namespace TextAnalysis {
             position.X = (int)(distanceFromCenter * Math.Sin(angle) + center.X);
             position.Y = (int)(distanceFromCenter * Math.Cos(angle) + center.Y);
 
-
-
             Definition d = TextAnalysis.Text.vectors[vector].definitions[definitionIndex];
 
+
+            //gets the size of this definition
+            Size definitionSize = new Size(0, 0);
+            int padding = 5;
+            //loops through all the vectors in this definitions
+            for (int i = 0; i < d.relatedVectors.Count; i++) {
+
+                //gets the size of the word (used to size the bubble)
+                string word = TextAnalysis.Text.vectors[d.relatedVectors[i]].word;
+                Size proposedSize = new Size(int.MaxValue, int.MaxValue);
+                TextFormatFlags flags = TextFormatFlags.NoPadding;
+                Size textSize = TextRenderer.MeasureText(g, word, f, proposedSize, flags);
+
+                //sets the sizes of this definition
+                if (textSize.Width > definitionSize.Width)
+                    definitionSize.Width = textSize.Width;
+                definitionSize.Height += textSize.Height + 3;
+            }
+
+            //draws all vectors in this definition
             int nextDrawHeight = 0;
-            int largestDrawWidth = 0;
             //loops through all the vectors in this definitions
             for (int i = 0; i < d.relatedVectors.Count; i++) {
 
                 //gets the word for this vector
                 string word = TextAnalysis.Text.vectors[d.relatedVectors[i]].word;
+                Point stringLocation = new Point(position.X - definitionSize.Width / 2, position.Y - definitionSize.Height / 2 + nextDrawHeight);
 
                 //gets the size of the word (used to size the bubble)
                 Size proposedSize = new Size(int.MaxValue, int.MaxValue);
                 TextFormatFlags flags = TextFormatFlags.NoPadding;
                 Size textSize = TextRenderer.MeasureText(g, word, f, proposedSize, flags);
-                Rectangle stringBound = new Rectangle(position.X, position.Y + nextDrawHeight, textSize.Width, textSize.Height);
+                Rectangle stringBound = new Rectangle(stringLocation.X, stringLocation.Y, textSize.Width, textSize.Height);
 
                 Brush b = Brushes.Black;
                 //if the mouse was hovering on this vector
@@ -101,21 +120,25 @@ namespace TextAnalysis {
                 }
 
                 //draws this word
-                g.DrawString(word, f, b, position.X, position.Y + nextDrawHeight);
+                g.DrawString(word, f, b, stringLocation.X, stringLocation.Y);
+                //g.DrawRectangle(Pens.Black, position.X, position.Y + nextDrawHeight, textSize.Width, textSize.Height);
 
-                //sets the sizes of this definition
-                nextDrawHeight += textSize.Height + 2;
-                if (textSize.Width > largestDrawWidth)
-                    largestDrawWidth = textSize.Width;
+                nextDrawHeight += textSize.Height + 3;
             }
 
-            g.DrawRectangle(Pens.Black, position.X, position.Y, largestDrawWidth, nextDrawHeight);
+            g.DrawRectangle(Pens.Black, position.X - definitionSize.Width / 2, position.Y - definitionSize.Height / 2, definitionSize.Width + padding, definitionSize.Height);
         }
 
         private void Visualisation_MouseMove(object sender, MouseEventArgs e) {
 
             mousePos = e.Location;
             Invalidate();
+        }
+
+        private void Visualisation_MouseClick(object sender, MouseEventArgs e) {
+            Visualisation v = new Visualisation(hoveredVect);
+            v.Show();
+            Hide();
         }
     }
 }
